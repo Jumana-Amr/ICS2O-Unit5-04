d8ff4b9106380c3f4c7a51d27a2eb86dea3f2ac9
JumanaAmr <jumana.amr@ocsbstudent.ca>
2021-02-25 00:45:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('button').addEventListener('click',report)
+document.getElementById('bg')
+.style.backgroundColor = 'purple'
+
+function report () {
+  alert(document.getElementById('input').value)
+}
